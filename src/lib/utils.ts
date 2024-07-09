c9706492eb566ae4e101d8e07fcebf6044a95e83
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-07-09 15:27:39 +0900

    🐛

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,9 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { createClient } from "microcms-js-sdk";
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error("Supabase URL or ANON KEY is not defined");
+}
 
 export const client = createClient({
   serviceDomain: "blog-next-myapp",
