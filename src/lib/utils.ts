8774624df8590851859ac2506b5538b6a73c7a76
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-07-09 15:18:48 +0900

    🐛

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,10 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { createClient } from "microcms-js-sdk";
 
+
 export const client = createClient({
   serviceDomain: "blog-next-myapp",
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY as string,
 });
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
